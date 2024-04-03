d5e6e993f680814b1a26757d67bf1af03cef68e5
ndubey2000 <ndubey.rewa2000@gmail.com>
2024-04-03 16:57:14 +0530

    initial commit

---
+++ src/test/java/com/shoppingproject/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
